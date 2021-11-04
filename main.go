24d308655b5666ad22c0437b43f0bfc292bb2170
AlexPiemont <piemont.alexander@gmail.com>
2021-11-04 15:50:00 +0100

    refactor to reduce iteration

--- main.go
+++ main.go
@@ -2,20 +2,22 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 type elevator struct {
-	Direction int
-	Floor     int
-	Capacity  int
-	Contents  []int
-	History   []int
+	Direction     int
+	Floor         int
+	Capacity      int
+	Contents      []int
+	History       []int
+	MaxFloorInDir int
 }
 
 type building struct {
-	El     elevator
-	Queues [][]int
+	El             elevator
+	Queues         [][]int
+	Floors         int
+	PeopleInQueues int
 }
 
 func NewBuilding(q [][]int, cap int) building {
@@ -27,37 +29,69 @@ func NewBuilding(q [][]int, cap int) building {
 	el.Capacity = cap
 	//elevator will always start going up
 	el.Direction = 1
+	//assume queue will have someone to pickup at or above floor 0
+	el.MaxFloorInDir = 0
 	bldg.El = el
+	bldg.Floors = len(q)
+	bldg.PeopleInQueues = 0
+	for i := 0; i < len(q); i++ {
+		if len(q[i]) > 0 {
+			bldg.PeopleInQueues += len(q[i])
+		}
+	}
+	bldg.getMaxFloor()
 	return bldg
+}
+
+func Abs(num int) int {
+	if num < 0 {
+		return -num
+	}
+	return num
+}
+func (b *building) getMaxFloor() {
+	for i := b.El.Floor + b.El.Direction; i < b.Floors && i >= 0; i += b.El.Direction {
+		if len(b.Queues[i]) > 0 {
+			b.El.MaxFloorInDir = i
+		}
+	}
 }
 
 func (b *building) moveElevator() {
 	stopAtFloor := false
+	elPop := len(b.El.Contents)
+	qPop := len(b.Queues[b.El.Floor])
+	//fmt.Println(qPop)
 	//Let people off from contents
-	if len(b.El.Contents) > 0 {
-		for i := 0; i < len(b.El.Contents); i++ {
+	if elPop > 0 {
+		for i := 0; i < elPop; i++ {
 			if b.El.Contents[i] == b.El.Floor {
 				b.El.Contents = append(b.El.Contents[:i], b.El.Contents[i+1:]...)
 				i--
+				elPop--
 				stopAtFloor = true
 			}
 		}
 	}
 	//Let people on who are going in the same direction
-	if len(b.Queues[b.El.Floor]) > 0 {
-		for i := 0; i < len(b.Queues[b.El.Floor]); i++ {
+	if qPop > 0 {
+		for i := 0; i < qPop; i++ {
 			//if the difference between desired and actual floor * direction is positive,
 			//then the person is going the same direction
 			//e.g. person going to 4 from 3 difference is 1. If direction is 1 then result is 1, positive
 			// person going to 3 from 4 difference is -1. if direction is -1 (down) then result is 1, positive
 			// mismatch in sign between difference and direction means negative result means not going same dir
 			if (b.Queues[b.El.Floor][i]-b.El.Floor)*b.El.Direction > 0 {
-				if len(b.El.Contents) < b.El.Capacity {
+				//fmt.Printf("i: %d", i)
+				if elPop < b.El.Capacity {
 					stopAtFloor = true
 					//add to elevator
 					b.El.Contents = append(b.El.Contents, b.Queues[b.El.Floor][i])
+					elPop++
 					//remove from queue
 					b.Queues[b.El.Floor] = append(b.Queues[b.El.Floor][:i], b.Queues[b.El.Floor][i+1:]...)
+					b.PeopleInQueues--
+					qPop--
 					i--
 				}
 			}
@@ -67,28 +101,9 @@ func (b *building) moveElevator() {
 	if stopAtFloor {
 		b.El.History = append(b.El.History, b.El.Floor)
 	}
-	//Check floors in direction for people
-	//using two flags here allows to only iterate through whole queue once per elevator iteration
-	//one checks if there are more people waiting at floors in the current direction
-	//one checks if the queues are empty entirely, and ready for the elevator to stop
-	peopleFoundInDir := false
-	peopleFoundInQueue := false
-	for i := 0; i < len(b.Queues); i++ {
-		if len(b.Queues[i]) > 0 {
-			peopleFoundInQueue = true
-			//if (b.El.Direction < 0 && i < b.El.Floor + b.El.Direction) || (b.El.Direction > 0 && i > b.El.Floor + b.El.Direction){
-			// if the floor with people (i) is in the direction of the elevator from El.Floor
-			// if the abs of difference between the current floor and the found floor is greater than the difference between the next floor and found floor
-			// if on 4 going down and found on 2 then 2 > 1
-			// if on 2 going up and found on 6 then -4 > -3
-			if math.Abs(float64(b.El.Floor-i)) > math.Abs(float64(b.El.Floor+b.El.Direction-i)) {
-				peopleFoundInDir = true
-				break
-			}
-		}
-	}
-	if !peopleFoundInDir && len(b.El.Contents) == 0 {
-		//if no people in direction && elevator empty switch direction
+	// if difference between floor and maxfloor times direction >= 0 then not yet at maxfloor
+	// if at or beyond maxfloor and elevator empty, switch directions
+	if (b.El.Floor-b.El.MaxFloorInDir)*b.El.Direction >= 0 && elPop == 0 {
 		//before switching direction, increment floor in direction so that iteration
 		// doesn't skip current floor
 		// Essentially, if this is not done, the elevator might stop at a floor with people
@@ -97,11 +112,13 @@ func (b *building) moveElevator() {
 		// the elevator to redo the drop-off/pick-up logic at the current floor with the new direction
 		b.El.Floor += b.El.Direction
 		b.El.Direction *= -1
+		//recalculate max floor with new direction
+		b.getMaxFloor()
 	}
 	// go to next floor in direction
 	b.El.Floor = b.El.Floor + b.El.Direction
 	// if more people need to be picked up or people remain on elevator, recurse
-	if peopleFoundInQueue || len(b.El.Contents) > 0 {
+	if b.PeopleInQueues > 0 || elPop > 0 {
 		b.moveElevator()
 	} else {
 		// if elevator both empty and queues empty
--- main_test.go
+++ main_test.go
@@ -18,7 +18,7 @@ func TestUp(t *testing.T) {
 	bldg := NewBuilding(queues, 5)
 	bldg.moveElevator()
 	if !reflect.DeepEqual(bldg.El.History, []int{0, 2, 5, 0}) {
-		t.Fatalf(`History doesn't match expectation`)
+		t.Fatalf(`Expectated: {0, 2, 5, 0} Got: %v`, bldg.El.History)
 	}
 }
 func TestDown(t *testing.T) {
@@ -34,7 +34,7 @@ func TestDown(t *testing.T) {
 	bldg := NewBuilding(queues, 5)
 	bldg.moveElevator()
 	if !reflect.DeepEqual(bldg.El.History, []int{0, 2, 1, 0}) {
-		t.Fatalf(`History doesn't match expectation`)
+		t.Fatalf(`Expectated: {0, 2, 1, 0} Got: %v`, bldg.El.History)
 	}
 }
 func TestUpAndUp(t *testing.T) {
@@ -50,7 +50,7 @@ func TestUpAndUp(t *testing.T) {
 	bldg := NewBuilding(queues, 5)
 	bldg.moveElevator()
 	if !reflect.DeepEqual(bldg.El.History, []int{0, 1, 2, 3, 4, 5, 0}) {
-		t.Fatalf(`History doesn't match expectation`)
+		t.Fatalf(`Expectated: {0, 1, 2, 3, 4, 5, 0} Got: %v`, bldg.El.History)
 	}
 }
 func TestDownAndDown(t *testing.T) {
@@ -67,5 +67,21 @@ func TestDownAndDown(t *testing.T) {
 	bldg.moveElevator()
 	if !reflect.DeepEqual(bldg.El.History, []int{0, 5, 4, 3, 2, 1, 0}) {
 		t.Fatalf(`Expectated: {0, 5, 4, 3, 2, 1, 0}, Got: %v`, bldg.El.History)
+	}
+}
+func TestDownAndDownWithCap(t *testing.T) {
+	queues := [][]int{
+		{},        // G
+		{0},       // 1
+		{},        // 2
+		{},        // 3
+		{2, 2, 2}, // 4
+		{3, 3},    // 5
+		{},        // 6
+	}
+	bldg := NewBuilding(queues, 3)
+	bldg.moveElevator()
+	if !reflect.DeepEqual(bldg.El.History, []int{0, 5, 4, 3, 2, 1, 0, 4, 2, 0}) {
+		t.Fatalf(`Expectated: {0, 5, 4, 3, 2, 1, 0, 4, 2, 0}, Got: %v`, bldg.El.History)
 	}
 }
